5ab0bd1fe5bffd5b44f279e4fd36a1c68f9a7aa5
KK1007 <kamalkrishanm@gmail.com>
2024-01-22 18:06:05 +0530

    title

---
+++ C++/first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+                    int a=90;
+                    cout<<&a;
+                    // &a it is used to print address
+                    int age;
+//    while declaring any variables we cant use space, symbols but we can use numbers and underscore.
+//    we cant start with numbers
+                    
+}
