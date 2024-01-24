ff475b91a6c0518af387a034caf21a19e4793ece
KK1007 <kamalkrishanm@gmail.com>
2024-01-24 18:38:40 +0530

    title

---
+++ C++/c_to_f.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+                    //  F = (9/5)C + 32
+                    float c,f;
+                   cout<<"enter temperatue in celcius -\t";
+                   cin>>c;
+                   f = (c*9/5) + 32;
+                   cout<<"\ntemperature in F="<<f;
+                   return 0;
+
+
+
+}
---
+++ C++/principal_intrest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+                    float p,r,t,si;
+                    cout<<"enter value of p=\t";
+                    cin>>p;
+                    cout<<"\nenter value of t=\t";
+                    cin>>t;
+                    cout<<"\nenter value of r=\t";
+                    cin>>r;
+                    si=p*r*t/100;
+                    cout<<"\nsimple intrest="<<si;
+                    return 0;
+
+}
