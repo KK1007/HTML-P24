7099838262f3863ebc16892b6500b93223f48bd2
KK1007 <kamalkrishanm@gmail.com>
2024-01-27 19:45:05 +0530

    title

--- C++/c_to_f.cpp
+++ C++/c_to_f.cpp
@@ -1,20 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-                    //  F = (9/5)C + 32
-//                     float c,f;
-//                    cout<<"enter temperatue in celcius -\t";
-//                    cin>>c;
-//                    f = (c*9/5) + 32;
-//                    cout<<"\ntemperature in F="<<f;
-//                    return 0;
-int a= 30 , b=20, c=19;
-b=c--;
-a= --b;
-c=b--;
-a=c--;
-cout<<a<<b<<c;
-
-
-
+                    // Formula -  F = (9/5)C + 32
+                    float c,f;
+                   cout<<"enter temperatue in celcius -\t";
+                    cin>>c;
+                   f = (c*9/5) + 32;
+                    cout<<"\ntemperature in F="<<f;
+                    return 0;
 }
