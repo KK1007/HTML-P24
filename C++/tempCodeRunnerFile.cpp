42e773508ade1d01e6b51822cfb55eb779366bf3
KK1007 <kamalkrishanm@gmail.com>
2024-01-31 19:23:45 +0530

    title

--- C++/tempCodeRunnerFile.cpp
+++ C++/tempCodeRunnerFile.cpp
@@ -1,11 +1,3 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-                    float a,b;
-                    cout<<"Enter first no\t";
-                    cin>>a;
-                    cout<<"Enter second no\t";
-                    cin>>b;
-                    cout<< "a+b="<<a+b;
-
-}
+int main()
