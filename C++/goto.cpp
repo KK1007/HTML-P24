2c2ddeee9c62cfa3c9c833e75537b037d88b6449
KK1007 <kamalkrishanm@gmail.com>
2024-02-07 17:53:01 +0530

    titile

---
+++ C++/goto.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+                    // cout<<"hello\n";
+                    // goto j;
+                    // cout<<"joy";
+                    // cout<<"hope";
+                    // j:
+                    // cout<<"byee";
+
+                    int a=1;
+                    prnt:
+                    cout<<a<<"\n";
+                    a++;
+                    if(a<=10)
+                    {
+                    goto prnt;
+                    }
+
+}
