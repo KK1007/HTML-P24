33dca3a14713ddf97c18be085fad261f70f97fc9
KK1007 <kamalkrishanm@gmail.com>
2024-02-19 22:52:38 +0530

    title

---
+++ C++/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+                    int a[5];
+                    a[0]=23;
+                    a[1]=3;
+                    a[2]=65;
+                    a[3]=90;
+                    a[4]=10;
+                    for(int i=0;i<5;++i)
+                    {
+                                        cout<<a[i]<<"\n";
+                    }
+}
