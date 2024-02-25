09c56ade29d6852af289972d19c61c9c694ea2c2
KK1007 <kamalkrishanm@gmail.com>
2024-02-25 16:29:49 +0530

    title

---
+++ java-script/area.js
@@ -0,0 +1,9 @@
+// let r=5;
+// console.log(3.14*r*r);
+var prompt= require('prompt-sync')();
+let p= parseInt(prompt("enter p"));
+let r= parseInt(prompt("enter r"));
+let t= parseInt(prompt("enter t"));
+intrest=(p*r*t)/100;
+console.log(intrest);
+
--- java-script/first.js
+++ java-script/first.js
@@ -1,1 +1,16 @@
-document.write("This is an external file of js")
+// var prompt= require('prompt-sync')();
+// let p= parseInt(prompt("enter p"));
+// let r= parseInt(prompt("enter r"));
+// let t= parseInt(prompt("enter t"));
+// intrest=(p*r*t)/100;
+// console.log(intrest);
+// document.write(intrest);
+
+let a= prompt("enter a");
+let b= prompt("enter b");
+if(a>b){
+                    document.write("a is greater than b");
+}
+else{
+                    document.write("b is greater than a");
+}
