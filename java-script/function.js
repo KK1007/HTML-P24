7dff18ea0625696c617dd87754b61db58b734928
KK1007 <kamalkrishanm@gmail.com>
2024-02-28 18:37:28 +0530

    title

---
+++ java-script/function.js
@@ -0,0 +1,25 @@
+// wap to find area of triangle
+// function area()
+// {
+//                    let l= parseInt(prompt("enter l"));
+//                    let b= parseInt(prompt("enter b"));
+//                    area= (l*b)/2;
+//                    document.write(area)
+// }
+// area();
+
+// wap to multiply 2 numbers without using multiplication operater
+// wap to add two number using function
+
+function sum()
+{
+                   let a=parseInt(prompt("enter a"));
+                   let b=parseInt(prompt("enter b"));
+                   document.write(a+b)
+}
+sum();
+
+
+
+
+
