e73271496ae83258a14e8e3d6b3dc5505cbefdbb
KK1007 <kamalkrishanm@gmail.com>
2024-03-07 17:56:33 +0530

    title

--- java-script/dom.js
+++ java-script/dom.js
@@ -2,9 +2,26 @@
 // a.innerHTML="hii";
 // console.log(a);
 
-function fun(){
-  let num1 =  parseInt(document.getElementById('n1').value);
-  let num2 =  parseInt(document.getElementById('n2').value);
+// function fun(){
+//   let num1 =  parseInt(document.getElementById('n1').value);
+//   let num2 =  parseInt(document.getElementById('n2').value);
 
-  alert(num1+num2);
-}
+//   let an =  document.getElementById('ans')
+//   an.innerHTML=num1+num2;
+// }
+
+// let an =  document.getElementsByClassName('c')
+//   an[1].innerHTML="website";
+//   an[1].style.backgroundColor="red";
+
+//  let a=document.getElementById('ul');
+//  a.style.display="flex";
+
+//  let a =document.getElementsByClassName('box');
+//  a[0].style.backgroundColor="red";
+//  a[0].style.height="200px";
+//  a[0].style.width="200px";
+
+
+let im = document.getElementById('im');
+im.src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_698,h_698/v1684855112/assets/96/4dd3d1-94e7-481e-b28c-08d59353b9e0/original/earner-illustra.png"
