4524c09f01c6ca468bcd46ab12c95a2980ee273e
KK1007 <kamalkrishanm@gmail.com>
2024-03-18 18:22:57 +0530

    title

--- java-script/array.js
+++ java-script/array.js
@@ -14,11 +14,28 @@
 // delete arr[1];
 // document.write(arr);
 
+
 // let arr = ['gfg', 'GFG', 'g', 'GeeksforGeeks'];
 // let namee = arr.pop();
 // document.write(namee+"<br>");
 // document.write(arr.length)
 
-origArray =[1,2,3,4,5,6];
-copiedArray=[...origArray];
-document.write(copiedArray); 
+// origArray =[1,2,3,4,5,6];
+// copiedArray=[...origArray];
+// document.write(copiedArray); 
+
+// let r=[1,2,3,4,5]
+// r.push(12,15)//used to put value in last position
+// r.pop()// used to delete last element
+// r.unshift(78)//used to insert value in first position
+// r.shift()//used to delete first element
+// document.write(r)
+
+
+
+let g =[4,5.5,6,10]
+let y = g.toString();
+let u = y[0]+y[2];
+document.write(u)
+ 
+           
